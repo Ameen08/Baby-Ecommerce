cc595de2c6f6ece58a83b89bf200769605e5b19a
aakhtar4 <16030059@lums.edu.pk>
2024-02-12 05:08:04 +0500

    fixed

--- Baby-Ecommerce/src/main/SellerInfo.java
+++ Baby-Ecommerce/src/main/SellerInfo.java
@@ -14,9 +14,11 @@ public String getSellerInformation(int productID)
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/baby-ecommerce","root","");
 			Statement stmt=con.createStatement();
 			ResultSet rs=stmt.executeQuery("SELECT seller.Name, seller.Location, products.Name FROM seller,products WHERE products.ID = seller.ProductID AND products.ID = '" + productID + "';");
-
-			rs.next();
-			String sellerInformation = " Seller Name: " + rs.getString(1) + " | Seller Location: " + rs.getString(2) + " | Sold Product Name: " + rs.getString(3);
+			String sellerInformation = "";
+			while(rs.next())
+			{
+				sellerInformation += " Seller Name: " + rs.getString(1) + " | Seller Location: " + rs.getString(2) + " | Sold Product Name: " + rs.getString(3) + "<BR>";
+			}
 			con.close();
 			
 			return sellerInformation;
